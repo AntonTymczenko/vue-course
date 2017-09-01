6b9f94ee3a7acd556207b32e77d566893415732b
Anton Tymczenko <tymczenko@protonmail.com>
2017-09-02 01:13:00 +0300

    start working with components

---
+++ webpack/src/Status.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+<div>
+  <p>Server status: {{ status }} <button @click="changeStatus">change</button></p>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      status: 'Critical'
+    }
+  },
+  methods: {
+    changeStatus () {
+      this.status = 'Normal'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
--- webpack/src/main.js
+++ webpack/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Status from './Status.vue'
+
+Vue.component('app-server-status', Status)
 
 new Vue({
   el: '#app',

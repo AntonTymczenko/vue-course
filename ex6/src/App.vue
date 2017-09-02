243ec51fb9c8f4b497904fb1ccc77cfdd33ad3ba
Anton Tymczenko <tymczenko@protonmail.com>
2017-09-02 23:10:02 +0300

    do exercise 6: components

---
+++ ex6/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="container">
+  <header>
+      <h1>Server Status</h1>
+  </header>
+  <hr>
+  <ul class="server-list">
+    <li v-for="index in 5">
+      <div class="row">
+        <div class="col-xs-12 col-sm-6">
+          <p>Server #{{ index }}</p>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+          <server-status></server-status>
+        </div>
+      </div>
+    </li>
+  </ul>
+  <hr>
+  <footer>
+    <p>All Servers are managed here</p>
+  </footer>
+</div>
+</template>
+
+<script>
+import ServerStatus from './ServerStatus.vue'
+
+export default {
+  components: {
+    'server-status': ServerStatus
+  }
+
+}
+</script>
+
+<style>
+.server-list {
+  list-style: none;
+}
+.server-list li {
+  padding: 10px;
+  border-bottom: 1px solid #ddd;
+}
+.server-list li:last-child {
+  border: none;
+}
+</style>
---
+++ ex6/src/ServerStatus.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+<div>
+  <p class="left">Server status: <span class='badge' :class="badgeType">{{ status }}</span> </p>
+  <button class="right btn" :class="btnType" v-if="status != 'Normal'" @click="changeStatus">Reanimate</button>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      status: 'Critical'
+    }
+  },
+  computed : {
+    badgeType() {
+      return {
+        'badge-danger': this.status == 'Critical' ? true : false,
+        'badge-success': this.status == 'Normal' ? true : false,
+      }
+    },
+    btnType() {
+      return {
+        'btn-danger': this.status == 'Critical' ? true : false
+      }
+    }
+  },
+  methods: {
+    changeStatus () {
+      this.status = 'Normal'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+.left {
+  display: block;
+  float: left;
+}
+.right {
+  display: block;
+  float: right;
+}
+</style>

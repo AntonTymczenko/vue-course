ca196d37003933e2218565abf22dfd0cb57c2cf4
Anton Tymczenko <tymczenko@protonmail.com>
2017-09-04 23:19:38 +0300

    learn components and slots

---
+++ ex7pre/src/components/New.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+<div>
+  <h3>new Quote</h3>
+  <button @click="counter++">increase</button>
+  <p>{{ counter }}</p>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      counter: 0
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>

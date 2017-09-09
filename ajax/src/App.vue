3b3d2f4330e0c6900a95e74e80f9153110b0c214
Anton Tymczenko <tymczenko@protonmail.com>
2017-09-09 15:20:43 +0300

    import vue-resource

---
+++ ajax/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="container">
+  <h1 class="text-center main">Vue-resource</h1>
+  <hr>
+  <div class="row text-center">
+    <div class="col-xs-6 mx-auto">
+
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+  }
+}
+</script>
+
+<style>
+</style>

bebf660b0571f1741cbb1090612771b6e7eb1cfc
Anton Tymczenko <tymczenko@protonmail.com>
2017-09-21 19:56:40 +0300

    started with donwloaded components folder

--- routing/src/App.vue
+++ routing/src/App.vue
@@ -4,20 +4,6 @@
   <hr>
   <div class="row text-center">
     <div class="col-xs-6 mx-auto">
-      <div class="form-group">
-        <label>Username</label>
-        <input type="text" class="form-control" v-model="user.username">
-      </div>
-      <div class="form-group">
-        <label>Mail</label>
-        <input type="email" class="form-control" v-model="user.email">
-      </div>
-      <button @click="submit" class="btn btn-primary">Submit</button>
-      <hr>
-      <button @click="fetch" class="btn btn-primary">Fetch</button>
-      <ul class="list-group">
-        <li class="list-group-item" v-for="u in users">{{ u.username }} – {{ u.email }}</li>
-      </ul>
 
     </div>
   </div>
@@ -28,36 +14,9 @@
 export default {
   data () {
     return {
-      user: {
-        username: '',
-        email: ''
-      },
-      users: []
     }
   },
   methods: {
-    submit() {
-      this.resource.save({}, this.user)
-        .then(resp => {
-          console.log(' -------- saved ------------')
-        })
-        .catch(e => {
-          if (JSON.parse(e.bodyText).message){
-            console.log(JSON.parse(e.bodyText).message)
-          } else {
-            console.log(JSON.parse(e.bodyText).errmsg)
-          }
-        })
-    },
-    fetch () {
-      this.$http.get('users')
-        .then(response => {
-          return response.json()
-        })
-        .then( data => {
-          this.users = data
-        })
-    }
   },
   created (){
     this.resource = this.$resource('', {}, {

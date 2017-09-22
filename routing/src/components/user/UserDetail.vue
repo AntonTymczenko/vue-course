177db304a64560c499f1dc9a02694813c8284751
Anton Tymczenko <tymczenko@protonmail.com>
2017-09-22 22:35:38 +0300

    query params add to UserEdit

--- routing/src/components/user/UserDetail.vue
+++ routing/src/components/user/UserDetail.vue
@@ -5,7 +5,11 @@
     <p>User of ID: {{ $route.params.id }}</p>
     <router-link
       tag="button"
-      :to="{name: 'userEdit', params: { id: $route.params.id}}"
+      :to="{
+        name: 'userEdit',
+        params: { id: $route.params.id},
+        query: {locale: 'en', q: 100}
+        }"
       class="btn btn-warning">Edit
     </router-link>
   </div>

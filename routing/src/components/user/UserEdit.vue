d8875e4ca41b33e1a39a5ad59d9db7ae1ad2472e
Anton Tymczenko <tymczenko@protonmail.com>
2017-09-23 01:42:06 +0300

    authorization in routing

--- routing/src/components/user/UserEdit.vue
+++ routing/src/components/user/UserEdit.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-    <h3>Edit the User</h3>
-    <hr>
-    <p>Locale: {{ $route.query.locale }}</p>
-    <p>Analytics: {{ $route.query.q }}</p>
-  </div>
+<div>
+  <h3>Edit the User</h3>
+  <hr>
+  <p>Locale: {{ $route.query.locale }}</p>
+  <p>Analytics: {{ $route.query.q }}</p>
+  <button class="btn btn-primary">Confirm</button>
+  <div style="height: 800px;"> </div>
+  <p id="data">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
 </template>

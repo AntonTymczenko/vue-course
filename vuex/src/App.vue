202b7ba3b3ae795e54967f244cf2d30864f9baf7
Anton Tymczenko <tymczenko@protonmail.com>
2017-09-23 20:14:39 +0300

    lesson 256

--- vuex/src/App.vue
+++ vuex/src/App.vue
@@ -4,6 +4,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Vuex</h1>
                 <app-result :counter="counter"></app-result>
+                <app-another-result :counter="counter"></app-another-result>
                 <hr>
                 <app-counter @updated="counter += $event"></app-counter>
             </div>
@@ -12,19 +13,20 @@
 </template>
 
 <script>
-    import Counter from './components/Counter.vue';
-    import Result from './components/Result.vue';
+import Counter from './components/Counter.vue';
+import Result from './components/Result.vue';
+import AnotherResult from './components/AnotherResult.vue';
 
-    export default {
-        data() {
-            return {
-                counter: 0
-            }
-        },
-        components: {
-            appCounter: Counter,
-            appResult: Result,
-        }
+export default {
+  data() {
+    return {
+      counter: 0
     }
+  },
+  components: {
+    appCounter: Counter,
+    appResult: Result,
+    appAnotherResult: AnotherResult
+  }
+}
 </script>
-
